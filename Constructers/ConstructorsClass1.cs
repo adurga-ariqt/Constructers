3abce9931506f601577522757fdad11d622dfb70
adurga-ariqt <adurga@ariqt.com>
2022-11-02 22:24:33 +0530

    Add project files.

---
+++ Constructers/ConstructorsClass1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructers
+{
+    public class ConstructorsClass1
+    {
+        int x;
+        int y;
+
+
+        public ConstructorsClass1()
+        {
+            x = 1;
+            y = 2;
+
+        }
+        public void Method()
+        {
+            int Z = x + y;
+            Console.WriteLine("z is :" + Z);
+
+
+        }
+    }
+}
---
+++ Constructers/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructers;
+
+Console.WriteLine("Hello, World!");
+ConstructorsClass1 obj = new ConstructorsClass1();
+obj.Method();
